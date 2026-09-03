f55465b61f880489c506a92f95555f54ed075e5a
srb241099 <136821168+srb241099@users.noreply.github.com>
2026-09-03 18:19:47 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='srb-games-v11';
+const CACHE='srb-games-v12';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install',event=>{
